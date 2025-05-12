a73e482a8f77a1c9961a67b0e50c6b5fffb4a19f
melissard02 <melissa.rd1258@gmail.com>
2025-05-12 11:05:44 -0600

    Debugging practice

---
+++ ponder_4/debugging.js
@@ -0,0 +1,18 @@
+const PI = 3.14;
+let area = 0;
+
+
+function circleArea(radius) {
+    let area = radius * radius * PI;
+    debugger;
+    return area;
+    debugger;
+}
+
+area = circleArea(3);
+console.log(area);
+
+area2 = circleArea(17);
+console.log(area2)
+
+console.log('Melissa Dickerson')
