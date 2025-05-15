99e30fb6dafd3f983f2897c0dd89438a4944f58f
melissard02 <melissa.rd1258@gmail.com>
2025-05-14 20:34:00 -0600

    Updated coolpics

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function menu() {
     const menuButton = document.getElementById("menu-toggle");
     const nav = document.getElementById("main-nav");
 
@@ -18,6 +18,50 @@ function handleResize()
         menuToggle.style.display = "block";
     }
 };
-
 window.addEventListener("resize", handleResize);
 handleResize();
+
+const galleryImages = document.querySelectorAll(".gallery img");
+galleryImages.forEach(function(image) 
+    {
+        image.addEventListener("click", function() 
+    {
+        showImageModal(image.src);
+    });
+});
+
+
+function showImageModal(smallSrc) {
+    const fileName = smallSrc.split("/").pop();
+    const nameParts = fileName.split("-");
+    const fullFileName = `${nameParts[0]}-full.jpeg`;
+    const fullSrc = `images/${fullFileName}`;
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("image-viewer");
+    dialog.innerHTML = `
+        <div class="modal-content">
+            <img class="full" src="${fullSrc}" alt="Pretty Cool Pic">
+            <button class="close-window">X</button>
+        </div>
+    `;
+
+    document.querySelector("main").appendChild(dialog);
+    dialog.showModal();
+
+    const closeButton = dialog.querySelector(".close-window");
+    closeButton.addEventListener("click", function() {
+        dialog.close();
+        dialog.remove();
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
