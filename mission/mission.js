93f03fc060dd2b164be90b192dad4dbb0d467a7b
melissard02 <melissa.rd1258@gmail.com>
2025-05-19 11:08:41 -0600

    Accessability changes

--- mission/mission.js
+++ mission/mission.js
@@ -1,16 +1,16 @@
 
-const changeTheme = document.getElementById("dark");
+const changeTheme = document.getElementById("dark-mode");
 const logo = document.getElementById("logo");
-const box = document.getElementsByClassName(".boxyboo");
+const box = document.getElementsByClassName(".missionbox");
 
 
 changeTheme.addEventListener("change", function() {
-    if (changeTheme.value == "dark") {
-        document.body.classList.add("dark");
+    if (changeTheme.value == "dark-mode") {
+        document.body.classList.add("dark-mode");
         logo.src = "images/byui-logo_white.png";
     }
     else {
-        document.body.classList.remove("dark");
+        document.body.classList.remove("dark-mode");
         logo.src = "images/byui-logo_blue.webp";
     }
 });
