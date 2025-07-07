c2d8168f8f98be90ed05be8be66c1b76d30ed6d6
melissard02 <melissa.rd1258@gmail.com>
2025-07-07 15:44:10 -0600

    Added a gitignore to keep repo clean and private information secure

--- dinosaurs/server.js
+++ dinosaurs/server.js
@@ -1,11 +1,39 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mysql = require('mysql2')
 
 app.get('/', (req, res) => {
   res.send('Hello from your Node/Express server!');
 });
 
 app.listen(PORT, () => {
-  console.log(`🔥 Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.get('/dinosaurs', (req, res) => {
+    connection.query('SELECT * FROM dinosaur', (err, results) => {
+        if (err) {
+            res.status(500).send('Error querying database');
+            return;
+        }
+        res.json(results);
+    })
+})
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to MySql:', err);
+        return;
+    }
+    console.log('Connected to MySQL database!')
+})
+
