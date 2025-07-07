12a39be0a623556a451fafca2b6fe2668f71f921
melissard02 <melissa.rd1258@gmail.com>
2025-07-07 11:07:32 -0600

    Added Node.js

---
+++ dinosaurs/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from your Node/Express server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🔥 Server is running at http://localhost:${PORT}`);
+});
