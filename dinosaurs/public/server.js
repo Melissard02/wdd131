863a17a26a54e59c5cff41513509128cb45895a9
melissard02 <melissa.rd1258@gmail.com>
2025-07-16 15:14:49 -0600

    Switched to psql

--- dinosaurs/public/server.js
+++ dinosaurs/public/server.js
@@ -3,41 +3,32 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const mysql = require('mysql2')
+const {Pool} = require('pg')
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello from your Node/Express server!');
+  res.send('Hello from Postgres + your Node/Express server!');
+});
+
+app.get('/dinosaurs', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM dinosaur');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('DB Error: ', err);
+        res.status(500).send('Error querying database');
+    }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-app.get('/dinosaurs', (req, res) => {
-    connection.query('SELECT * FROM dinosaur', (err, results) => {
-        if (err) {
-            res.status(500).send('Error querying database');
-            return;
-        }
-        res.json(results);
-    })
-})
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySql:', err);
-        return;
-    }
-    console.log('Connected to MySQL database!')
-})
-
